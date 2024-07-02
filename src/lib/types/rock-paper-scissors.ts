169d0c1a26ed83acfe7e569d74a83853a8b1f26d
Berzan <berzan@tutanota.com>
2024-07-02 15:09:19 +0300

    Define rock, paper, scissors types

---
+++ src/lib/types/rock-paper-scissors.ts
@@ -0,0 +1,11 @@
+export enum RockPaperScissorsChoice {
+    Rock = 'rock',
+    Paper = 'paper',
+    Scissors = 'scissors',
+}
+
+export enum RockPaperScissorsResult {
+    Won = 'won',
+    Lost = 'lost',
+    Draw = 'draw',
+}
